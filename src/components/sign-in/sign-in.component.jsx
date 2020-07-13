e7d236c0b30c1220d153b553fdef65ec4135838a
MANROOP PARMAR <manroopsingh120@gmail.com>
2020-07-13 17:44:31 +0530

    Sign up handle changes

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './sign-in.styles.scss';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
-import { SignInWithGoogle } from '../../firebase/firebase.utils.js';
+import { auth , SignInWithGoogle } from '../../firebase/firebase.utils';
 
 class SignIn extends React.Component {
 
@@ -16,9 +16,18 @@ class SignIn extends React.Component {
     }
 
 
-    handleSubmit = event => {
+    handleSubmit = async event => {
         event.preventDefault();
-        this.setState({ email : '', password : ''})
+
+        const {email , password} = this.state;
+
+        try{
+            await auth.signInWithEmailAndPassword( email , password); 
+            this.setState({ email : '', password : ''});
+        }catch(error){
+            console.log(error);
+        }
+        
     }
 
     handleChange = event => {
