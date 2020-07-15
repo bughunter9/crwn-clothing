267a17d2a3079b3133add79974066a34782b0d09
MANROOP PARMAR <manroopsingh120@gmail.com>
2020-07-15 15:36:28 +0530

    Stripe payments integration

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_51H567xFPDR0Zkj8J13cGLVRLUh3IaJ44cEBcPnLzJI5NO39jlkCbtgQAdn32U80qhopCc6LAE235yvvUxRC8TIPx00oGiztFDe';
+
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Successful!');
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='CRWN Clothing Ltd.'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
